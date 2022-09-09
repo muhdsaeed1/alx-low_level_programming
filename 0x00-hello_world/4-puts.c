892ba8d0abbf9e91c7d22bc50b0cb84fda0e5d7e
muhdsaeed1 <olayinkasaheed32@gmail.com>
2022-09-08 21:27:19 -0700

    programming is multilingual

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
-
 /**
 
-* main - print out  the string using the put function
+* main - print out  
 *
-* Description : using the main function
-* this program prints " programming is like building a multilingual puzzle "
 * Return: always 0 (success)
 */
 int main(void)
