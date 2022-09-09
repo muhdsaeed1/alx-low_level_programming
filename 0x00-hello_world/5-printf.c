5046a430ebfb78645825d204c356e57037b2e9a5
muhdsaeed1 <olayinkasaheed32@gmail.com>
2022-09-08 22:37:49 -0700

    proper art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+/**
+* main-print the string in the put function
+*
+* Description using the main function 
+*this program puts "programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+
+printf ( "with proper grammer, but the outcome is a piece of art,\n");
+return(0);
+}
