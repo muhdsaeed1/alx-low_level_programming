9f39860ecf0f336d0a51732a08e42ddea2559ebb
muhdsaeed1 <olayinkasaheed32@gmail.com>
2022-09-09 02:31:05 -0700

    fourth

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include <studio.h>
+#include <stdio.h>
 /**
-* main - entry point
-*
-*Return: Always 0 (Success)
-*/
-int main(void)
-{
-puts("\"programming is like building a multilingual puzzle");
-return (0);
-}
+ * main - Entry point
+ * Return: Always 0 (Success)
+ * */
+int main(void) 
+	{
+		puts("\"Programming is like building a multilingual puzzle"); return (0);
+		}
+
