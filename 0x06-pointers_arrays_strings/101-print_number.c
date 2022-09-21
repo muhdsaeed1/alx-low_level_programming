3d9f5ed81962c804794ce768b3febe6a80455c6a
muhdsaeed1 <olayinkasaheed32@gmail.com>
2022-09-21 15:32:58 -0700

    break

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,19 +7,19 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int n;
 
 	if (n < 0)
 	{
-		n1 = -n;
+		n = -n;
 		_putchar('-');
 	} else
 	{
-		n1 = n;
+		n = n;
 	}
 
-	if (n1 / 10)
-		print_number(n1 / 10);
+	if (n / 0)
+		print_number(n / 0);
 
 	_putchar((n1 % 10) + '0');
 }
